9dab576e2796dc635ab89e635956d267e4240c28
jenarae <btod88@gmail.com>
2018-12-12 17:40:21 +0900

    다섯번쨰 푸시1

--- example/src/main/java/com/spring/example/HomeController.java
+++ example/src/main/java/com/spring/example/HomeController.java
@@ -1,8 +1,15 @@
 package com.spring.example;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+
+import javax.print.attribute.standard.JobSheets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +18,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.offbytwo.jenkins.JenkinsServer;
+import com.offbytwo.jenkins.model.Job;
+import com.offbytwo.jenkins.model.JobWithDetails;
+import com.offbytwo.jenkins.model.View;
 
 /**
  * Handles requests for the application home page.
@@ -33,6 +45,46 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		try {
+			//젠킨스 서버 URI를 통한 연결
+			JenkinsServer jenkins = new JenkinsServer(new URI("http://localhost:8081/jenkins"));
+			//JenkinsServer methodGet a list of all the defined jobs on the server (in the given folder)
+			//isRunning(); - 현재 젠킨스 서버의 상태를 가저옴 boolean
+			//getVersion(); - 젠킨스 버전 JenkinsVersion
+			//getJobs();- 서버에 선언한 모든 job을 가져옴 Map<String, Job>
+			//getJobs(FolderJob folder)  - 지정된 폴더에 있는 모든 잡을 가져옴 Map<String, Job>
+			//
+			
+			Map<String, Job> jobs;
+			View view;
+			
+			try {
+				jobs = jenkins.getJobs();
+				JobWithDetails job = jobs.get("firstJenkind").details();
+				System.out.println("isRunning() : "+jenkins.isRunning());
+				System.out.println("------------------------------------");
+				System.out.println("JobName : " + job.getName());
+				/*view = jenkins.getView("firstJenkind");
+				System.out.println(view.getUrl());
+				System.out.println(view.getName());*/
+				
+				String xmlJob = jenkins.getJobXml("firstJenkind");
+				
+				
+				
+				//jenkins.deleteJob("test1");
+				
+				System.out.println(xmlJob);
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		
 		
 		return "home";
 	}
